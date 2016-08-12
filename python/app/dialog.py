db7a83a6ec28abccd7d5ce8c04df218169c49a6c
Kirill Kovalevskiy <kovalewskiy@gmail.com>
2016-08-12 11:33:22 -0700

    Working version that list all Deliveries

--- python/app/dialog.py
+++ python/app/dialog.py
@@ -1,11 +1,11 @@
 # Copyright (c) 2013 Shotgun Software Inc.
-# 
+#
 # CONFIDENTIAL AND PROPRIETARY
-# 
-# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit 
+#
+# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit
 # Source Code License included in this distribution package. See LICENSE.
-# By accessing, using, copying or modifying this work you indicate your 
-# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights 
+# By accessing, using, copying or modifying this work you indicate your
+# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
 # not expressly granted therein are reserved by Shotgun Software Inc.
 
 import sgtk
@@ -36,41 +36,43 @@ def show_dialog(app_instance):
 
     # we pass the dialog class to this method and leave the actual construction
     # to be carried out by toolkit.
-    app_instance.engine.show_dialog("Starter Template App...", app_instance, AppDialog)
+    app_instance.engine.show_dialog("Consolidator App", app_instance, AppDialog)
 
 class AppDialog(QtGui.QWidget):
     """
     Main application dialog window
     """
-    
+
     def __init__(self):
         """
         Constructor
         """
         # first, call the base class and let it do its thing.
         QtGui.QWidget.__init__(self)
-        
+
         # now load in the UI that was created in the UI designer
-        self.ui = Ui_Dialog() 
+        self.ui = Ui_Dialog()
         self.ui.setupUi(self)
-        
+
         # most of the useful accessors are available through the Application class instance
         # it is often handy to keep a reference to this. You can get it via the following method:
         self._app = sgtk.platform.current_bundle()
-        
+
         # via the self._app handle we can for example access:
         # - The engine, via self._app.engine
         # - A Shotgun API instance, via self._app.shotgun
-        # - A tk API instance, via self._app.tk 
-        
+        # - A tk API instance, via self._app.tk
+
         # setup our data backend
         self._model = shotgun_model.SimpleShotgunModel(self)
+
+        # import pdb; pdb.set_trace()
 
         # tell the view to pull data from the model
         self.ui.view.setModel(self._model)
 
         # load all assets from Shotgun
-        self._model.load_data(entity_type="Asset")
+        self._model.load_data(entity_type="Delivery", fields=['title', 'type', 'id'])
 
         # setup a delegate
         self._delegate = ListItemDelegate(self.ui.view)
